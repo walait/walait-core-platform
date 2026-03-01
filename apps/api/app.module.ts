2088565e45d9fab69be87666477a41154a959476
Walait <walatech1990@gmail.com>
2026-02-28 21:49:32 -0300

    chore(app): register TenisModule in api app

--- apps/api/app.module.ts
+++ apps/api/app.module.ts
@@ -1,19 +1,20 @@
 import { ConfigModule, ConfigService } from "@nestjs/config";
 
-import { APP_PIPE } from "@nestjs/core";
 import { AppController } from "@/app.controller";
 import { AppService } from "@/app.service";
-import { EventBusModule } from "@/shared/event-bus/event-bus.module";
-import { EventEmitterModule } from "@nestjs/event-emitter";
-import { Module } from "@nestjs/common";
-import { SharedModule } from "@/shared/shared.module";
-import { TaxesModule } from "@/modules/taxes/taxes.module";
-import { TypeOrmModule } from "@nestjs/typeorm";
-import { WebhookModule } from "@/modules/webhook/webhook.module";
-import { ZodValidationPipe } from "nestjs-zod";
-import { getTypeOrmConfig } from "@/shared/database/typeorm.config";
 import whatsappConfig from "@/config/whatsapp.config";
+import { TaxesModule } from "@/modules/taxes/taxes.module";
+import { TenisModule } from "@/modules/tenis/tenis.module";
+import { WebhookModule } from "@/modules/webhook/webhook.module";
+import { getTypeOrmConfig } from "@/shared/database/typeorm.config";
+import { EventBusModule } from "@/shared/event-bus/event-bus.module";
+import { SharedModule } from "@/shared/shared.module";
+import { Module } from "@nestjs/common";
+import { APP_PIPE } from "@nestjs/core";
+import { EventEmitterModule } from "@nestjs/event-emitter";
+import { TypeOrmModule } from "@nestjs/typeorm";
 import { LoggerModule } from "nestjs-pino";
+import { ZodValidationPipe } from "nestjs-zod";
 
 @Module({
   imports: [
@@ -40,6 +41,7 @@ import { LoggerModule } from "nestjs-pino";
     EventBusModule,
     EventEmitterModule.forRoot(),
     WebhookModule,
+    TenisModule,
   ],
   controllers: [AppController],
   providers: [
